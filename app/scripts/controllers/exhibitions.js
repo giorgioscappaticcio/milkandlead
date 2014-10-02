ec990c58dc6704e33c48811f58bb6cfbdba45750
giorgioscappaticcio <ooohh1@hotmail.it>
2014-10-02 18:18:56 +0100

    update

--- app/scripts/controllers/exhibitions.js
+++ app/scripts/controllers/exhibitions.js
@@ -8,10 +8,54 @@
  * Controller of the milkandleadApp
  */
 angular.module('milkandleadApp')
-  .controller('ExhibitionsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ExhibitionsCtrl', function ($scope, wpapi) {
+    
+  	// SubNAV Control
+	$scope.subnav = ['front','pictures', 'info'];
+    $scope.selection = $scope.subnav[0];
+
+
+  	// Call the service wpapi.js
+	var getExhibiotns = wpapi.getExhibitions();
+
+	getExhibiotns.then(function(greeting) {
+		console.log(greeting);
+		$scope.exhibObj = greeting.posts;
+	}, function(reason) {
+	  alert('Failed: ' + reason);
+	}, function(update) {
+	  alert('Got notification: ' + update);
+	});
+
+
+	$scope.set_flyerBg = function (i) {
+		var fbBanner = $scope.exhibObj[i].custom_fields["wpcf-facebook-banner"];
+		if (fbBanner != undefined){
+			return { 'background-image': 'url('+$scope.exhibObj[i].custom_fields["wpcf-facebook-banner"][0]+')' }
+		}
+	}
+
+
+	String.prototype.splice = function( idx, rem, s ) {
+	    return (this.slice(0,idx) + s + this.slice(idx + Math.abs(rem)));
+	};
+
+	$scope.spiceThumb = function(thumb){
+		switch (thumb.split('.').pop()){
+			case 'jpeg' :
+				return thumb.splice(-5, 0, "-150x150");
+			break;
+			case 'jpg' :
+				return thumb.splice(-4, 0, "-150x150");
+			break;
+			case 'png' :
+				return thumb.splice(-4, 0, "-150x150");
+			break;
+			case 'gif' :
+				return thumb.splice(-4, 0, "-150x150");
+			break;
+		}
+	}
+
+
   });
